1e3f350121995586f16ad8538f386877ad8f7e1a
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2026-02-05 20:56:15 -0300

    refactor: remove arquivos duplicados e não utilizados - limpa estrutura do projeto removendo 26 arquivos (rotas, controllers, páginas e arquivos órfãos)

--- api/test-whatsapp.js
+++
@@ -1,8 +0,0 @@
-import { sendWhatsappMessage } from './services/whatsapp.service.js';
-
-await sendWhatsappMessage(
-  '+5543988146622',
-  '🚀 TESTE REAL: WhatsApp Twilio Sandbox funcionando!'
-);
-
-process.exit();
